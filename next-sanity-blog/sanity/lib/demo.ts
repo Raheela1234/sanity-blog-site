00f7db4fadf6a38b2085954519eb3acd97f71982
Shahid Ali <153486921+Shahid12ali@users.noreply.github.com>
2025-01-15 02:30:02 +0500

    blog-site-withsanity

---
+++ next-sanity-blog/sanity/lib/demo.ts
@@ -0,0 +1,48 @@
+/**
+ * Demo data used as placeholders and initial values for the blog
+ */
+
+export const title = "Cars Blog";
+
+export const description = [
+  {
+    _key: "9f1a629887fd",
+    _type: "block",
+    children: [
+      {
+        _key: "4a58edd077880",
+        _type: "span",
+        marks: [],
+        text: "A statically generated blog example using ",
+      },
+      {
+        _key: "4a58edd077881",
+        _type: "span",
+        marks: ["ec5b66c9b1e0"],
+        text: "Next.js",
+      },
+      {
+        _key: "4a58edd077882",
+        _type: "span",
+        marks: [],
+        text: " and ",
+      },
+      {
+        _key: "4a58edd077883",
+        _type: "span",
+        marks: ["1f8991913ea8"],
+        text: "Sanity",
+      },
+      {
+        _key: "4a58edd077884",
+        _type: "span",
+        marks: [],
+        text: ".",
+      },
+    ],
+    
+    style: "normal",
+  },
+];
+
+export const ogImageTitle = "A Next.js Blog with a Native Authoring Experience";
